a435e34678ce4e2b7eda8788ae4ebe78469c3529
ANtonio <malavolta4@gmail.com>
2020-06-11 18:01:25 -0400

    frist commit

---
+++ src/components/Transaction.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <div>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}
+        Cash{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
+        <button className="delete-btn">x</button>
+      </li>
+    </div>
+  );
+};
