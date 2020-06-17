315ae1a1d925cb74badec31a74c701e6dd480505
ANtonio <malavolta4@gmail.com>
2020-06-17 19:00:45 -0400

    get all transactions for api

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,9 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 //Inital state
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Create context
@@ -12,6 +15,21 @@ export const GlobalContext = createContext(initialState);
 // Provider componet
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+  async function getTransactions() {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
+    }
+  }
 
   // Actions
   function deleteTransaction(id) {
@@ -32,8 +50,11 @@ export const GlobalProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
         deleteTransaction,
         addTransaction,
+        getTransactions,
       }}
     >
       {children}
